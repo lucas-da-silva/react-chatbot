f320c38238ebb7408fd033f69c627ee94e701edf
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-29 11:16:09 -0300

    feat: create ChatBot context and provider

---
+++ src/app/provider/ChatBotContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const ChatBotContext = createContext();
+
+export default ChatBotContext;
---
+++ src/app/provider/index.js
@@ -0,0 +1,7 @@
+import ChatBotContext from './ChatBotContext';
+import ChatBotProvider from './ChatBotProvider';
+
+export {
+  ChatBotContext,
+  ChatBotProvider,
+};
