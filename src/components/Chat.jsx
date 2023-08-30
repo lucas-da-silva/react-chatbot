5f48f5f0c45ecc5cf8896cb014de35dafaea2c65
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-30 12:17:16 -0300

    feat: implements functions handleSubmit and renderBotMessage in Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
-
+import React, { useContext, useState } from 'react';
 import {
   Card, CardHeader, CardTitle, CardContent, CardFooter, CardDescription,
 } from './ui/card';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { ScrollArea } from './ui/scroll-area';
+import BotCardContent from './BotCardContent';
+import BotMessageWithOptions from './BotMessageWithOptions';
+import UserMessage from './UserMessage';
+import { ChatContext } from '../provider';
+import BotMessageWithReference from './BotMessageWithReference';
 
 export default function Chat() {
+  const { messages, sendMessage } = useContext(ChatContext);
+  const [chatInput, setChatInput] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    sendMessage(chatInput);
+    setChatInput('');
+  };
+
+  const renderBotMessage = (message) => {
+    if (message.options) {
+      return (
+        <BotMessageWithOptions
+          key={message.id}
+          message={message.content}
+          options={message.options}
+        />
+      );
+    }
+    if (message.reference) {
+      return (
+        <BotMessageWithReference
+          key={message.id}
+          message={message.content}
+          reference={message.reference}
+        />
+      );
+    }
+
+    return <BotCardContent key={message.id}>{message.content}</BotCardContent>;
+  };
+
   return (
     <Card className="w-[440px]">
       <CardHeader>
@@ -17,30 +52,20 @@ export default function Chat() {
       </CardHeader>
       <CardContent>
         <ScrollArea className="h-[600px] w-full pr-4 mt-2">
-          <div className="flex gap-2 text-[#1a0400] font-medium text-sm mb-4">
-            <Avatar className="self-end h-6 w-6">
-              <AvatarFallback>BOT</AvatarFallback>
-              <AvatarImage src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/512px-ChatGPT_logo.svg.png" />
-            </Avatar>
-            <div className="bg-zinc-100 p-3 rounded-r-2xl rounded-t-2xl">
-              <p className="leading-relaxed">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, doloribus fugit? Porro explicabo ducimus distinctio, enim, laboriosam cum similique officiis debitis, modi earum aut exercitationem! Totam ullam soluta voluptatibus quos!
-              </p>
-            </div>
-          </div>
-          <div className="flex text-sm mb-4 justify-end text-[#1a0400] font-medium">
-            <div className="bg-[#fae5ca] p-3 rounded-l-xl rounded-t-xl">
-              <p className="leading-relaxed">
-                Qual a
-              </p>
-            </div>
-          </div>
-
+          {
+            messages.map((message) => (message.role === 'assistant'
+              ? renderBotMessage(message)
+              : <UserMessage key={message.id} message={message} />))
+          }
         </ScrollArea>
       </CardContent>
       <CardFooter>
-        <form className="w-full flex gap-2">
-          <Input placeholder="How can I help you?" />
+        <form onSubmit={handleSubmit} className="w-full flex gap-2">
+          <Input
+            placeholder="How can I help you?"
+            value={chatInput}
+            onChange={(e) => setChatInput(e.target.value)}
+          />
           <Button type="submit">Send</Button>
         </form>
       </CardFooter>
